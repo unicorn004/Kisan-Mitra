2547f03c203e32cf97aa1bfc9564584213cbbe6a
Tanmay Sinkar <tanmaysinkar2021@gmail.com>
2024-04-27 15:53:00 +0530

    chnage

--- KisanMitra/asgi.py
+++ KisanMitra/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for resumehub project.
+ASGI config for KisanMitra project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resumehub.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'KisanMitra.settings')
 
 application = get_asgi_application()
